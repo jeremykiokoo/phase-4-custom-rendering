eda4026e597e74f96bf328aa9190ad83e6018b68
jeremykioko <jeremy.kioko@student.moringaschool.com>
2023-07-03 10:21:51 +0300

    complete

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -1,7 +1,20 @@
-class Cheese < ApplicationRecord
-  
-  def summary
-    "#{name}: $#{price}"
+class CheesesController < ApplicationController
+
+  # GET /cheeses
+  def index
+    cheeses = Cheese.all
+    render json: cheeses, only[:id, :name], methods[:sumary]
+  end
+
+  # GET /cheeses/:id
+  def show
+
+    cheese = Cheese.find_by(id: params[:id])
+    if cheese
+     render json: cheese
+    else
+      render json: {error: "cheese not found"}, status: :not_found
+    end
   end
 
 end
